abd37817f340b394317deb4f71569210a4110ead
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-01-11 10:07:32 -0600

    commit tree 0b2b2b7d8698b831454acc9d0da45414a7988bcd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(active = "roller-skate") {
+    return (`This Saturday, I want to ${active}!`);
+}
+
+const mondayWork = function (todo = 'go to the office') {
+    return `This Monday, I will ${todo}.`;
+}
+
+function wrapAdjective (flair = '*') {
+    return function (param = 'special') {
+        return `You are ${flair}${param}${flair}!`;
+    };
+}
